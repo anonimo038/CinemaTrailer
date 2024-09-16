95c032a8b2f007a79f705ec2c09817ac1092471a
JulianP <juliandavidpedraza26@gmail.com>
2024-09-15 19:51:41 -0500

    parte 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,79 @@
-import { useEffect, useState } from 'react'
-import React from 'react'
-import axios from 'axios'
-import YouTube from 'react-youtube';
+import { useEffect, useState } from "react";
+import React from "react";
+import axios from "axios";
+import YouTube from "react-youtube";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-  const API_URL = 'https://api.themoviedb.org/3'
-  const API_KEY = 'a3e8ee7fc1bb17b4c6581324b0cd34c6'
-  const IMAGE_PATH = 'https://image.tmdb.org/t/p/original/wwemzKWzjKYJFfCeiB57q3r4Bcm.svg'
-  const URL_IMAGE = 'https://image.tmdb.org/t/p/original/'
+  const API_URL = "https://api.themoviedb.org/3";
+  const API_KEY = "a3e8ee7fc1bb17b4c6581324b0cd34c6";
+  const IMAGE_PATH =
+    "https://image.tmdb.org/t/p/original/wwemzKWzjKYJFfCeiB57q3r4Bcm.svg";
+  const URL_IMAGE = "https://image.tmdb.org/t/p/original/";
 
-//VARIABLES DE ESTADO 
-const [movies , setMovies]= useState([])
-const [searchKey, setSearchKey]= useState("")
-const [trailer, setTrailer] = useState(null);
-const [movie, setMovie]= useState({tittle : "loading  peliculas"});
-const [playing, setPlaying]= useState(false);
+  //VARIABLES DE ESTADO
+  const [movies, setMovies] = useState([]);
+  const [searchKey, setSearchKey] = useState("");
+  const [trailer, setTrailer] = useState(null);
+  const [movie, setMovie] = useState({ tittle: "loading  peliculas" });
+  const [playing, setPlaying] = useState(false);
 
+  //esta funcion realiza la peticion por GET a la api
+  const fetchMovies = async (searchKey) => {
+    const type = searchKey ? "search" : "discover";
+    const {
+      data: { results },
+    } = await axios.get(`${API_URL}/${type}/movie`, {
+      params: {
+        api_key: API_KEY,
+        query: searchKey,
+      },
+    });
+    setMovies(results);
+    setMovie(results[0]);
+  };
 
-//esta funcion realiza la peticion por GET a la api
-const fetchMovies = async(searchKey)=>{
-  const type =searchKey ? "search" : "discover";
-  const {data: {results},
+  // funcion para buscar peliculas
+  const searchMovies = (e) => {
+    e.preventDefault();
+    fetchMovies(searchKey);
+  };
 
-} = await axios.get(`${API_URL}/${type}/movie`,{
-  params: {
-    api_key : API_KEY,
-    query: searchKey,
-  },
-
-
-
-});
-setMovies(results)
-setMovie(results[0])
-};
-
-
-
-useEffect(()=>{
-  fetchMovies();
-},[])
+  useEffect(() => {
+    fetchMovies();
+  }, []);
 
   return (
-    <div>
-      
-      {/* contenedor que va mostra poster de peliculas */ }
-      <div className='container mt-3'>
+    <div className="text-center ">
+      <h2 className="text-center mt-5 mb-5">Trailer Movies </h2>
+      {/*Buscador */}
+      <form className="container mb-4" onSubmit={searchMovies}>
+        <input
+          type="text"
+          placeholder="search"
+          onChange={(e) => setSearchKey(e.target.value)}
+        />
+        <button className="btn btn-primary">Search</button>
+      </form>
+      {/* contenedor que va mostra poster de peliculas */}
+      <div className="container  mt-2">
         <div className="row">
-          {movies.map((movie)=>(
-            <div key={movie.id} className='col-md-4 mb-3'>
-              <img src={`${URL_IMAGE + movie.poster_path}`} alt='' height={600} width="100%"></img>
-              <h4 className='text-center'>{movie.title}</h4>
+          {movies.map((movie) => (
+            <div key={movie.id} className="col-md-4 mb-3">
+              <img
+                src={`${URL_IMAGE + movie.poster_path}`}
+                alt=""
+                height={600}
+                width="100%"
+              ></img>
+              <h4 className="text-center">{movie.title}</h4>
             </div>
           ))}
         </div>
-
       </div>
     </div>
   );
 }
-
 
 export default App;
